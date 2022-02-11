54e26941d8446fe0f995d9f5bf370568181dd847
Vitalii Kniazev <13.kn.v.v@gmail.com>
2022-02-11 17:22:47 +0300

    Drag’n’Drop beter

--- js/script.js
+++ js/script.js
@@ -3,30 +3,47 @@ const cl = (massege) => console.log(massege)
 const cd = (massege) => console.dir(massege)
 
 /////////////////////////
-// task: Persistent Bugger
+// task: 
 
-// solve task: Persistent Bugger
-const cont = document.querySelectorAll('.cont') 
-const test = document.querySelector('.test')
+// solve task: 
+const cont = document.querySelector('.cont') 
+const tests = document.querySelectorAll('.test')
 
 
-for (let test of cont) {
+for (let test of tests) {
     test.addEventListener('pointerdown', pointerdown)
+    test.ondragstart = () => false
 }
 
 function pointerdown(e) {
-    if (e.target.closest('.test')) {
-        e.target.addEventListener('pointermove', pointermove)        
-    }   
+        e.target.addEventListener('pointermove', pointermove)
+        e.target.setPointerCapture(e.pointerId) 
 }
 
 function pointermove(e) {
-    const eRect = e.target.closest('.cont').getBoundingClientRect()
-    e.target.style.left =  e.clientX - eRect.x -e.target.offsetWidth/2 +'px'
-    e.target.style.top =  e.clientY - eRect.y -e.target.offsetHeight/2 +'px'
+    const contRect = cont.getBoundingClientRect()
+ 
+    e.target.style.left =  e.clientX - contRect.x - e.target.offsetWidth/2 +'px'
+    e.target.style.top =  e.clientY - contRect.y - e.target.offsetHeight/2 +'px'
+    
+    if (e.target.style.left < 0+'px') {
+        e.target.style.left = 0 +'px'
+    }
+
+    if (contRect.left + e.target.offsetLeft + e.target.offsetWidth  > contRect.right -3) {
+        e.target.style.left = contRect.right - contRect.left - e.target.offsetWidth - 3 +'px'
+    }
+
+    if (e.target.style.top < 0+'px') {
+        e.target.style.top = 0 +'px'
+    }
+
+    if (contRect.top + e.target.offsetTop + e.target.offsetHeight  > contRect.bottom -3) {
+        e.target.style.top = contRect.bottom - contRect.top - e.target.offsetHeight - 3 +'px'
+    }
+
     e.target.addEventListener('pointerup', deletePointermove)
-    e.target.addEventListener('pointerleave', deletePointermove)
-}
+ }   
 
 function deletePointermove(e) {
     e.target.removeEventListener('pointermove', pointermove)
