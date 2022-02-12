7936437936a17ba5a9cf95cc007a44fee947593c
Vitalii Kniazev <13.kn.v.v@gmail.com>
2022-02-13 00:55:28 +0300

    Solved task: Two Sum

--- js/script.js
+++ js/script.js
@@ -3,41 +3,35 @@ const cl = (massege) => console.log(massege)
 const cd = (massege) => console.dir(massege)
 
 /////////////////////////
-// task: Count of positives / sum of negatives 
-//
-// Given an array of integers.
+// task: Two Sum 
+//Write a function that takes an array of numbers (integers for the tests)
+// and a target number. It should find two different items in the array that,
+// when added together, give the target value. The indices of these items
+// should then be returned in a tuple like so: (index1, index2).
 
-// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// For the purposes of this kata, some tests may have multiple answers; 
+// any valid solutions will be accepted.
 
-// If the input array is empty or null, return an empty array.
-
-// Example
-// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+// The input will always be valid (numbers will be an array of length 2 or 
+// greater, and all of the items will be numbers; target will always be
+// the sum of two different items from that array).
 // solve task: 
 
-function countPositivesSumNegatives(input) {
-    let quantity = 0
-    let sum = 0
-    
-    if (input == undefined || input.length === 0  ) return []
-
-    input.forEach(num => {
-        if (num > 0) quantity += 1
-        if (num < 0) sum += num
-    })
-
-    if (quantity === 0 && sum ===0) return []
-
-    return [quantity, sum]
+function twoSum(numbers, target) {
+    for (let i = 0; i < numbers.length; i++) {
+        for (let k = i+1; k < numbers.length; k++) {
+            if (numbers[i] + numbers[k] === target) {
+                return [i, k]
+            }
+        }
+    }
 }
     
 
 
-cl( countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
-// [10, -65];
-cl( countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]))
-//[8, -50];
-cl( countPositivesSumNegatives())
-//[]
-cl( countPositivesSumNegatives([0,0]))
-//[]
+cl(twoSum([1,2,3], 4))
+// [0, 2];
+cl(twoSum([1234,5678,9012], 14690))
+//[1, 2];
+cl(twoSum([2,2,3], 4))
+//[0, 1]
