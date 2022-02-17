a11096789ecfa70165d7607f5ce2ad500faad7ae
Vitalii Kniazev <13.kn.v.v@gmail.com>
2022-02-17 20:03:54 +0300

    mouse: added turns

--- js/script.js
+++ js/script.js
@@ -3,61 +3,8 @@ const cl = (massege) => console.log(massege)
 
 /////////////////////////
 ////////////////////////
-// task: Rot13
-///ROT13 is a simple letter substitution cipher that replaces a letter 
-// with the letter 13 letters after it in the alphabet. ROT13 is an example
-//  of the Caesar cipher.
+// task: 
+ 
 
-// Create a function that takes a string and returns the string ciphered 
-// with Rot13. If there are numbers or special characters included in the string, 
-// they should be returned as they are. Only letters from the latin/english alphabet 
-// should be shifted, like in the original Rot13 "implementation".
 // solve task: 
 
-function rot13(message) {
-    let alphabetUpperCase = []
-    let alphabetLowerCase = []
-    
-    for (let i = 65; i <= 90; i++) {
-        alphabetUpperCase.push(String.fromCodePoint(i))
-    }
-    for (let i = 97; i <= 122; i++) {
-        alphabetLowerCase.push(String.fromCodePoint(i))
-    }
-
-    function change(x) {
-
-        if (x.codePointAt() >= 65 && x.codePointAt() <= 90) {
-            let indexX = alphabetUpperCase.indexOf(x)
-            let indexXPlus13 = alphabetUpperCase.indexOf(x) + 13
-            if (indexX + 13 > alphabetUpperCase.length-1) {
-                indexXPlus13 = 12 - (alphabetUpperCase.length-1 - indexX)
-            }
-            return alphabetUpperCase[indexXPlus13]
-        }
-
-        if (x.codePointAt() >= 97 && x.codePointAt() <= 122) {
-            let indexX = alphabetLowerCase.indexOf(x)
-            let indexXPlus13 = alphabetLowerCase.indexOf(x) + 13
-            if (indexX + 13 > alphabetLowerCase.length-1) {
-                indexXPlus13 = 12 - (alphabetLowerCase.length-1 - indexX)
-            }
-            return alphabetLowerCase[indexXPlus13]
-        }
-
-        return x
-    }
-
-    return message.split('')
-                  .map(x => change(x))
-                  .join('')
-  }
-
-
-
-
-  cl(rot13("test"))//"grfg"
-  cl(rot13("Test"))//"Grfg"
-  cl(rot13("Ruby is cool!"))//Ehol vf pbby!
-  cl(rot13("aaAA"))//nnNN
- 
--- mouse/mouse.js
+++ mouse/mouse.js
@@ -5,9 +5,11 @@ const cl = (massege) => console.log(massege)
 ////////////////////////
 
 const mouse = document.getElementById('mouse')
+const mouseImg = document.querySelectorAll('.mouse__img') 
+const mouseEnter = document.querySelector('.mouse__enter')
 let speed = 5
 const stepSpeed = 5
-const maxSpeed = 100
+const maxSpeed = 50
 const minSpeed = 3
 const speedScroll = speed
 
@@ -16,48 +18,98 @@ document.body.addEventListener('keydown', mouseStart)
 document.body.addEventListener('keyup', mouseStop)
 
 function mouseStart(e) {
+    wavTail()
     if (speed < maxSpeed) speed += stepSpeed
-    
-    if (e.code === 'KeyD') {
-        let left = mouse.offsetLeft
-        mouse.style.left = left + speed +'px'
-        if (left + mouse.offsetWidth > document.documentElement.clientWidth  - 11) {
-            mouse.style.left = document.documentElement.clientWidth - mouse.offsetWidth - 10 +'px'
-        }
-    }
-    if (e.code === 'KeyA') {
-        let left = mouse.offsetLeft
-        mouse.style.left = left - speed +'px'
-        if (left < 11) {
-            mouse.style.left = 10 +'px'
-        }
-    }
-    if (e.code === 'KeyW') {
-        let top = mouse.offsetTop
-        mouse.style.top = top - speed +'px'
-        if (top < 11) {
-            mouse.style.top = 10 +'px'
-        }
-    }
-    if (e.code === 'KeyS') {
-        let top = mouse.offsetTop
-        mouse.style.top = top + speed +'px'
-        if (top + mouse.offsetHeight > document.documentElement.clientHeight  - 11) {
-            mouse.style.top = document.documentElement.scrollTop + 
-                                document.documentElement.clientHeight - 
-                                mouse.offsetHeight - 10 +'px'
-            window.scrollTo(0, window.pageYOffset + speedScroll)
-        }
-    }
-    
+    if (e.code === 'KeyS') goDown()
+    if (e.code === 'KeyW') goUp()
+    if (e.code === 'KeyA') goLeft()
+    if (e.code === 'KeyD') goRight()
+    if (e.code === 'Enter') mouseClick()
+
 }
 
 function mouseStop (e) {
     speed = minSpeed
 }
+/////////////////////////////
+function wavTail() {
+    Array.from(mouseImg).forEach(img => {
+        img.classList.toggle('mouse__img_tail-change')
+    })
+}
 
-for (let i = 0; i < 100; i++) {
-    let p = document.createElement('p')
-    p.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, praesentium voluptatem cumque distinctio beatae quod repellat eaque possimus quisquam quidem vitae dolores? Itaque atque distinctio, culpa voluptatem praesentium provident dolore!'
-    document.querySelector('body').append(p)
+
+
+function goUp() {
+    let top = mouse.offsetTop
+    
+    mouseRotate(180)
+
+    mouse.style.top = top - speed +'px'
+
+    if (top  < 11) {
+        mouse.style.top = 10 +'px'
+    }
+    if (top < window.pageYOffset ) {
+           window.scrollBy(0, -speedScroll)
+    }
+}
+
+function goLeft() {
+    let left = mouse.offsetLeft
+
+    mouseRotate(90)
+    mouse.style.left = left - speed +'px'
+
+    if (left < 11) {
+        mouse.style.left = 0 +'px'
+    }
+}
+
+function goDown() {
+    let top = mouse.offsetTop
+
+    mouseRotate(0)
+    mouse.style.top = top + speed +'px'
+
+    if (top + mouse.offsetHeight > document.documentElement.clientHeight + 
+        window.pageYOffset - 20)  {
+         mouse.style.top = document.documentElement.scrollTop + 
+                        document.documentElement.clientHeight - 
+                        mouse.offsetHeight - 10 +'px'
+        window.scrollBy(0, speedScroll)
+    }
+}
+
+function goRight() {
+    let left = mouse.offsetLeft
+
+    mouseRotate(-90)
+    
+    mouse.style.left = left + speed +'px'
+    if (left + mouse.offsetWidth > document.documentElement.clientWidth  - 11) {
+        mouse.style.left = document.documentElement.clientWidth - mouse.offsetWidth - 10 +'px'
+    }
+}
+
+function mouseRotate(deg) {
+    if (mouse.style.transform == `rotate(${deg}deg)`) return
+    mouse.style.transform = `rotate(${deg}deg)` 
+}
+
+function mouseClick() {
+    
+    let x = mouseEnter.getBoundingClientRect().x 
+    let y = mouseEnter.getBoundingClientRect().y
+    mouse.hidden = true
+    let element = document.elementFromPoint(x, y)
+    mouse.hidden = false
+    let event = new MouseEvent("click")
+    element.dispatchEvent(event)
+    if (element.matches('input')) {
+        element.focus()
+    } // else element.blur()
+    ////////// не работает element.blur()
+    
+    cl(element)
 }
