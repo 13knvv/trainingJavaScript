f824ac4d8ec0afc3c1f02a76a0855facc4f01789
Vitalii Kniazev <13.kn.v.v@gmail.com>
2022-02-18 03:05:20 +0300

    paint: added smoothness

--- paint/paint.js
+++ paint/paint.js
@@ -2,19 +2,16 @@
 
 const canvas = document.getElementById('paint__canvas')
 const ctx = canvas.getContext('2d')
-const positionCanvas = canvas.getBoundingClientRect()
 const inputColor = document.querySelector('.paint__color')
 const inputSize = document.querySelector('.paint__size')
-let color = 'black'
-let size = '4'
-ctx.lineCap = "round"
+
 
 inputColor.addEventListener('input', (e) => {
-    color = e.target.value
+    ctx.strokeStyle  = e.target.value
 })
 
 inputSize.addEventListener('input', (e) => {
-    size = e.target.value
+    ctx.lineWidth = e.target.value
 })
 
  canvas.addEventListener('mousedown', () => {
@@ -25,10 +22,20 @@ inputSize.addEventListener('input', (e) => {
  })
 
  function draw(e) {
-    let x = e.offsetX;
-    let y = e.offsetY;
-    
-    ctx.fillRect(x/4.76 + x*0.0025, y/4.76+ y*0.0025, size, size)
-    ctx.fillStyle = color
-    ctx.fill()
-}
+    let x = e.offsetX/4.76 + e.offsetX*0.0025
+    let y = e.offsetY/4.76 + e.offsetY*0.0025
+    let dx = e.movementX/4.76// + e.movementX*0.0025
+    let dy = e.movementY/4.76 //+ e.movementX*0.0025
+
+    ctx.lineCap = "round";    
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x - dx, y - dy);
+    ctx.stroke();
+    ctx.closePath();
+}  
+
+//     // очистка изображения
+//     document.getElementById("clear").onclick = function clear() {
+//       ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     };
